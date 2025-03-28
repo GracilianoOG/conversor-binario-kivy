378ef7338714fcb21fd7aced0b1b27ab31b27e6f
Gabriel <gabriel.gracilianomb@gmail.com>
2025-03-28 12:19:46 -0300

    Add window size and background color

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+
+
+Window.size = (360, 580)
+Window.clearcolor = (0.98, 0.52, 0, 1)
 
 
 class WindowLayout(BoxLayout):

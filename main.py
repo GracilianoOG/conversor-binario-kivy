9f6724e0c5b2b9c8d294208dcf7636714d68b536
Gabriel <gabriel.gracilianomb@gmail.com>
2025-03-29 22:58:36 -0300

    Move conversion logic to to_binary

--- main.py
+++ main.py
@@ -10,31 +10,33 @@ Window.clearcolor = (0.98, 0.52, 0, 1)
 class WindowLayout(FloatLayout):
     def clear_output(self):
         self.ids.input.text = ""
-    
+
     def on_convert_click(self):
-        ogDecimal = self.ids.input.text
-        decimal = ogDecimal
+        self.to_binary(self.ids.input.text)
+    
+    def to_binary(self, decimal):
+        decimalCalc = decimal
         binary = ""
 
-        if(decimal != ""):
+        if(decimalCalc != ""):
+            decimalCalc = int(decimalCalc)
             decimal = int(decimal)
-            ogDecimal = int(ogDecimal)
         else:
             self.ids.output.text = "Digite um valor para conversão!"
             return
 
-        if(decimal == 0):
+        if(decimalCalc == 0):
             binary = "0"
-        elif(decimal < 0):
+        elif(decimalCalc < 0):
             self.ids.output.text = "Apenas números positivos!"
             self.clear_output()
             return
 
-        while decimal >= 1:
-            binary = str(decimal % 2) + binary
-            decimal = decimal // 2
+        while decimalCalc >= 1:
+            binary = str(decimalCalc % 2) + binary
+            decimalCalc = decimalCalc // 2
 
-        self.ids.output.text = f"Valor {ogDecimal} em binário:\n{binary.zfill(8)}"
+        self.ids.output.text = f"Valor {decimal} em binário:\n{binary.zfill(8)}"
         self.clear_output()
 
 
